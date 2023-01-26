807a2b2cead1689d711435c7d75f16a42c7d26cb
SRIHARSHA ROUTHU <69401277+telnet-harsha@users.noreply.github.com>
2023-01-27 00:31:19 +0530

    Update python packages (#841)
    * Update python packages

--- kubestash/__init__.py
+++ kubestash/__init__.py
@@ -12,6 +12,8 @@ import copy
 import traceback
 from collections import namedtuple
 
+import collections
+collections.Callable = collections.abc.Callable
 
 # TODO: args.profile, args.arn
 # TODO: args.version
